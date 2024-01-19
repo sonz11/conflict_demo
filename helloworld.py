083c88cb81523052862c36bea8cc99081f3f554a
Sonia lee <sonialeeying@gmail.com>
2024-01-19 17:52:06 +0800

    add helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,9 @@
-def hello_world(city, state):
-    print(f'Hello World {city}, {state}!')
+def hello_world():
+    print(f'Hello World!')
 
 
 def main():
-    hello_world('NYC', 'New York')
+    hello_world()
 
 
 main()
